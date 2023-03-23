50dd733ce6db1f5718419127b14d9a4c3314d8b8
lk3v1nn <kevinlmorataya@gmail.com>
2023-03-22 21:42:47 -0600

    funcion que separa

--- leer txt 3/arrastrarArchivo.js
+++ leer txt 3/arrastrarArchivo.js
@@ -20,24 +20,24 @@ input.addEventListener('change', ()=>{
 
 //Evento que se activa cuando arrastre adentro de areaParaArrastrar
 areaParaArrastrar.addEventListener('dragover', (e)=>{
-//    e.preventDefault();
+    e.preventDefault();
     areaParaArrastrar.classList.add('activo');
     TextP.innerHTML = 'Suelta para carga el archivo';
 });
 //Evento que se activa cuando arrastre afuera de areaParaArrastrarraArrastrar
 areaParaArrastrar.addEventListener('dragleave', (e)=>{
-//    e.preventDefault();
+    e.preventDefault();
     areaParaArrastrar.classList.remove('activo');
     TextP.innerHTML = 'Arrastra aqui';
 });
 //Evento se activa cunado suelto un elemento dentro de areaParaArrastrar
 areaParaArrastrar.addEventListener('drop', (e) =>{
-//    e.preventDefault();
+    e.preventDefault();
     areaParaArrastrar.classList.remove('activo');
     TextP.innerHTML = 'Archivo cargado';
 });
 
-todo.addEventListener('drop', (e)=>{
-     e.preventDefault();
+todo.addEventListener('drop', (e) =>{
+    e.preventDefault();
 });
 
